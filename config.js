350d8a177b1771b79cb6d9f4ebb7849176d0fb1a
JalanJiang <448300947@qq.com>
2019-10-15 23:04:49 +0800

    refactor(*): 修改配置与模板

--- config.js
+++ config.js
@@ -1,25 +1,28 @@
 module.exports = {
 	cdnDomain: '/',
-	title: 'Zing Gallery',
-	avatar: './img/avatar.png',
+	title: '时光',
+	avatar: './img/avatar.jpeg',
 	wording: {
 		noAccess: '抱歉，你没有权限访问'
 	},
 	albums: {
-		"青海湖": {
-			thumbnail: "打马而过.jpg",
-		  sort: 1
+		"薯条": {
+			description: "2018 年我有了人生的第一只小猫咪。",
+			thumbnail: "IMG_9166.JPG",
+		  	sort: 1
 		}, 
-		"江南小镇": {
-		  description : "没有什么能够阻挡",
-			thumbnail: "燕歌行.jpg",
-		  name: "南浔"
+		"20191013 D2307": {
+			description: "🚄D2307，往返福州南与深圳北，是和谐号 CRH1A 型电力动车组，设计速度为 250 km/h。",
+			thumbnail: "IMG_3532.JPG",
+		  	name: "2019.10.13 福州南 -> 深圳北"
 		},
-		"私密": {
-			description: "这是一个需要密码的相册",
-		  name: "私密",
-		  password: "233",
-			passwordTips: "密码是233"
-		}
+		"香港": {
+
+		},
+		"福州地铁": {
+			description: "穿越榕城两千年。",
+			thumbnail: "IMG_08932.JPG",
+			name: "福州地铁"
+		},
 	}
 }
